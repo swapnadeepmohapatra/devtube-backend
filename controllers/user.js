81c753d71272c50e10868415f1c7bf04dda6364a
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-05-11 18:51:08 +0530

    added video controller

--- controllers/user.js
+++ controllers/user.js
@@ -16,7 +16,7 @@ exports.getUserById = (req, res, next, id) => {
 	User.findById(id).exec((error, user) => {
 		if (error || !user) {
 			return res.status(400).json({
-				message: 'No user found',
+				error: 'No user found',
 			});
 		}
 		req.profile = user;
---
+++ controllers/video.js
@@ -0,0 +1,37 @@
+const Video = require('../models/VideoD');
+
+exports.saveVideo = (req, res) => {
+	const video = new Video(req.body);
+
+	video.save((err, video) => {
+		if (err) {
+			return res.status(400).json({ error: 'Video Upload Failed' });
+		}
+
+		return res.status(200).json(video);
+	});
+};
+
+exports.getAllVideos = (req, res) => {
+	Video.find()
+		.populate('writer')
+		.exec((err, videos) => {
+			if (err) {
+				return res.status(400).json({ error: err });
+			}
+
+			return res.status(200).json({ videos });
+		});
+};
+
+exports.getVideoById = (req, res) => {
+	Video.findById(req.body.videoId)
+		.populate('writer')
+		.exec((err, video) => {
+			if (err) {
+				return res.status(400).json({ error: err });
+			}
+
+			return res.status(200).json({ video });
+		});
+};
