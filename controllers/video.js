8fea7ab58c9bbcb51c1aae8b3e8f75f2200e2ef2
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-05-14 22:38:11 +0530

    added getSubscriptionVideos

--- controllers/video.js
+++ controllers/video.js
@@ -1,4 +1,5 @@
 const Video = require('../models/VideoD');
+const Subscriber = require('../models/SubscriberD');
 
 exports.saveVideo = (req, res) => {
 	const video = new Video(req.body);
@@ -33,5 +34,25 @@ exports.getVideoById = (req, res) => {
 			}
 
 			return res.status(200).json({ video });
+		});
+};
+
+exports.getSubscriptionVideos = (req, res) => {
+	Subscriber.find({ userFrom: req.body.userFrom, userTo: req.body.userTo }).exec((err, subscribers) => {
+		let subscribedChannels = [];
+
+		subscribers.map((subscriber, i) => {
+			subscribedChannels.push(subscriber.userTo);
+		});
+	});
+
+	Video.find({ write: { $in: subscribedChannels } })
+		.populate('writer')
+		.exec((err, videos) => {
+			if (err) {
+				return res.status(400).json({ error: err });
+			}
+
+			res.status(200).json({ videos });
 		});
 };
