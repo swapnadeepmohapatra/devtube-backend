084e50cbc6628634436f3ba608dc0d8fe47aebc2
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2020-05-26 16:39:19 +0530

    resolved like bugs

--- controllers/like.js
+++ controllers/like.js
@@ -1,60 +1,65 @@
-const LikeD = require('../models/LikeD');
-const DislikeD = require('../models/DislikeD');
+const LikeD = require("../models/LikeD");
+const DislikeD = require("../models/DislikeD");
 
 exports.getLike = (req, res) => {
-	let likeID = {};
+  let likeID = {};
+  console.log(req.body);
 
-	if (req.body.videoId) {
-		lk = { videoId: req.body.videoId };
-	} else {
-		lk = { videoId: req.body.commentId };
-	}
+  if (req.body.videoId) {
+    likeID = { videoId: req.body.videoId };
+  } else {
+    likeID = { videoId: req.body.commentId };
+  }
 
-	LikeD.find(likeID).exec((err, likes) => {
-		if (err) {
-			return res.status(400).json({ error: err });
-		}
+  LikeD.find(likeID).exec((err, likes) => {
+    if (err) {
+      return res.status(400).json({ error: err });
+    }
 
-		return res.status(200).json({ likes });
-	});
+    return res.status(200).json({ likes });
+  });
 };
 
 exports.incLike = (req, res) => {
-	let likeData = {};
-	if (req.body.videoId) {
-		likeData = { videoId: req.body.videoId, userId: req.body.userId };
-	} else {
-		likeData = { commentId: req.body.commentId, userId: req.body.userId };
-	}
+  let likeData = {};
+  console.log(req.body);
 
-	const like = new LikeD(likeData);
+  if (req.body.videoId) {
+    likeData = { videoId: req.body.videoId, userId: req.body.userId };
+  } else {
+    likeData = { commentId: req.body.commentId, userId: req.body.userId };
+  }
 
-	like.save((err, likeResult) => {
-		if (err) {
-			return res.json({ error: err });
-		}
+  const like = new LikeD(likeData);
 
-		DislikeD.findOneAndDelete(variable).exec((err, disLikeResult) => {
-			if (err) {
-				return res.status(400).json({ error: err });
-			}
-			res.status(200).json({ likeResult });
-		});
-	});
+  like.save((err, likeResult) => {
+    if (err) {
+      return res.json({ error: err });
+    }
+
+    DislikeD.findOneAndDelete(likeData).exec((err, disLikeResult) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      res.status(200).json({ likeResult });
+    });
+  });
 };
 
 exports.decLike = (req, res) => {
-	let likeData = {};
-	if (req.body.videoId) {
-		likeData = { videoId: req.body.videoId, userId: req.body.userId };
-	} else {
-		likeData = { commentId: req.body.commentId, userId: req.body.userId };
-	}
+  console.log(req.body);
 
-	LikeD.findOneAndDelete(likeData).exec((err, result) => {
-		if (err) {
-			return res.status(400).json({ error: err });
-		}
-		res.status(200).json({ result });
-	});
+  let likeData = {};
+  if (req.body.videoId) {
+    likeData = { videoId: req.body.videoId, userId: req.body.userId };
+  } else {
+    likeData = { commentId: req.body.commentId, userId: req.body.userId };
+  }
+
+  LikeD.findOneAndDelete(likeData).exec((err, result) => {
+    if (err) {
+      return res.status(400).json({ error: err });
+    }
+    res.status(200).json({ result });
+  });
 };
--- controllers/video.js
+++ controllers/video.js
@@ -1,71 +1,84 @@
-const Video = require('../models/VideoD');
-const Subscriber = require('../models/SubscriberD');
+const Video = require("../models/VideoD");
+const Subscriber = require("../models/SubscriberD");
 
 exports.saveVideo = (req, res) => {
-	const video = new Video(req.body);
+  const video = new Video(req.body);
 
-	video.save((err, video) => {
-		if (err) {
-			return res.status(400).json({ error: err });
-		}
+  video.save((err, video) => {
+    if (err) {
+      return res.status(400).json({ error: err });
+    }
 
-		return res.status(200).json({ video });
-	});
+    return res.status(200).json({ video });
+  });
 };
 
 exports.getAllVideos = (req, res) => {
-	Video.find()
-		.populate('writer')
-		.exec((err, videos) => {
-			if (err) {
-				return res.status(400).json({ error: err });
-			}
+  Video.find()
+    .populate("writer")
+    .exec((err, videos) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
 
-			return res.status(200).json({ videos });
-		});
+      return res.status(200).json({ videos });
+    });
 };
 
 exports.getVideoById = (req, res) => {
-	Video.findById(req.body.videoId)
-		.populate('writer')
-		.exec((err, video) => {
-			if (err) {
-				return res.status(400).json({ error: err });
-			}
+  Video.findById(req.body.videoId)
+    .populate("writer")
+    .exec((err, video) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
 
-			return res.status(200).json({ video });
-		});
+      return res.status(200).json({ video });
+    });
 };
 
 exports.getSubscriptionVideos = (req, res) => {
-	Subscriber.find({ userFrom: req.body.userFrom, userTo: req.body.userTo }).exec((err, subscribers) => {
-		let subscribedChannels = [];
+  Subscriber.find({
+    userFrom: req.body.userFrom,
+    userTo: req.body.userTo,
+  }).exec((err, subscribers) => {
+    let subscribedChannels = [];
 
-		subscribers.map((subscriber, i) => {
-			subscribedChannels.push(subscriber.userTo);
-		});
-	});
+    subscribers.map((subscriber, i) => {
+      subscribedChannels.push(subscriber.userTo);
+    });
+  });
 
-	Video.find({ write: { $in: subscribedChannels } })
-		.populate('writer')
-		.exec((err, videos) => {
-			if (err) {
-				return res.status(400).json({ error: err });
-			}
+  Video.find({ write: { $in: subscribedChannels } })
+    .populate("writer")
+    .exec((err, videos) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
 
-			res.status(200).json({ videos });
-		});
+      res.status(200).json({ videos });
+    });
 };
 
 exports.getRecommendedVideos = (req, res) => {
-	Video.find({ _id: { $ne: req.body.videoId } })
-		.limit(5)
-		.populate('writer')
-		.exec((err, videos) => {
-			if (err) {
-				return res.status(400).json({ error: err });
-			}
+  let videoId = req.body.videoId;
 
-			return res.status(200).json({ videos });
-		});
+  Video.findOneAndUpdate({ _id: videoId }, { $inc: { views: 1 } }).exec(
+    (err, vdo) => {
+      if (err) {
+        console.log(err);
+      }
+      Video.find({ _id: { $ne: req.body.videoId } })
+        .sort({ views: -1 })
+        .limit(5)
+        .populate("writer")
+        .exec((err, videos) => {
+          if (err) {
+            return res.status(400).json({ error: err });
+          }
+
+          return res.status(200).json({ videos });
+        });
+    }
+  );
 };
